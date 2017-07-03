1a28ea3930165ae4ca58b3dffec793cd0d4e326f
Pancor <klawisz338@gmail.com>
2017-07-03 18:22:54 +0200

    Added automated app version name and version code

--- app/src/main/java/pancor/pl/ztmgdansk/MainActivity.java
+++ app/src/main/java/pancor/pl/ztmgdansk/MainActivity.java
@@ -1,7 +1,10 @@
 package pancor.pl.ztmgdansk;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PackageManager manager = this.getPackageManager();
+        PackageInfo info = null;
+        try {
+            info = manager.getPackageInfo(this.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this,
+                "PackageName = " + info.packageName + "\nVersionCode = "
+                        + info.versionCode + "\nVersionName = "
+                        + info.versionName + "\nPermissions = " + info.permissions, Toast.LENGTH_SHORT).show();
     }
 }
