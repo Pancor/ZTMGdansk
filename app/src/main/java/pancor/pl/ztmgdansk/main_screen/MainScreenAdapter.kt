3ba78529d3101cf1f5204077b34cf51b92d34871
Pancor <klawisz338@gmail.com>
2017-07-04 16:43:49 +0200

    Created base for recycler adapter

---
+++ app/src/main/java/pancor/pl/ztmgdansk/main_screen/MainScreenAdapter.kt
@@ -0,0 +1,32 @@
+package pancor.pl.ztmgdansk.main_screen
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pancor.pl.ztmgdansk.R
+
+
+class MainScreenAdapter(val items: List<RecyclerActionItem>) :
+        RecyclerView.Adapter<MainScreenAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent?.context)
+        val view = inflater.inflate(R.layout.act_main_screen, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return items[position].viewType
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
---
+++ app/src/main/java/pancor/pl/ztmgdansk/main_screen/RecyclerActionItem.kt
@@ -0,0 +1,4 @@
+package pancor.pl.ztmgdansk.main_screen
+
+
+data class RecyclerActionItem(val viewType: Int = -1, val title: String, val imageId: Int)
