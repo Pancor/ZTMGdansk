51307d15b584b4d44b78495b7f279f237405fa19
Pancor <klawisz338@gmail.com>
2017-07-27 16:24:05 +0200

    added new fields to models classes

--- app/src/main/java/pancor/pl/ztmgdansk/models/BusStop.kt
+++ app/src/main/java/pancor/pl/ztmgdansk/models/BusStop.kt
@@ -4,4 +4,22 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 @Entity
-data class BusStop(@PrimaryKey val stopId: Int)
+data class BusStop(@PrimaryKey val stopId: Int,
+                   val stopName: String){
+
+    constructor(stopId: Int,
+                stopCode: Int,
+                stopName: String,
+                stopShortName: Int,
+                stopDesc: String,
+                subName: String,
+                date: String,
+                latitude: Double,
+                longitude: Double,
+                zoneId: Int,
+                nonPassenger: Int,
+                depot: Int,
+                ticketZoneBorder: Int,
+                onDemand: Int,
+                activationDate: String) : this(stopId, stopName)
+}
